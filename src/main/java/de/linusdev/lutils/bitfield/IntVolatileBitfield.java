a020dcf365898926e25e00ece21591477209d531
LinusDev <info.linusdev@gmail.com>
2023-04-02 15:26:32 +0200

    HTTP Stuff WIP added Bitfield (LAPI-13)

--- src/main/java/de/linusdev/lutils/bitfield/IntVolatileBitfield.java
+++ src/main/java/de/linusdev/lutils/bitfield/IntVolatileBitfield.java
@@ -6,6 +6,10 @@ package de.linusdev.lutils.bitfield;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Bitfield class using a volatile int. It is not thread safe!
+ * @param <V> field value
+ */
 @SuppressWarnings({"NonAtomicOperationOnVolatileField", "unused"})
 public class IntVolatileBitfield<V extends LongBitFieldValue> {
 
@@ -23,34 +27,67 @@ public class IntVolatileBitfield<V extends LongBitFieldValue> {
         this.value = value;
     }
 
+    /**
+     * reset this bitfield to 0
+     */
     public void reset() {
         this.value = 0;
     }
 
+    /**
+     * checks if given flag is set.
+     * @param flag flag to check
+     * @return {@code true} if given flag is set in this bitfield.
+     */
     public boolean isSet(@NotNull V flag) {
         return (value & flag.getValue()) == flag.getValue();
     }
 
+    /**
+     * checks if given flag is set.
+     * @param flag flag to check
+     * @return {@code true} if given flag is set in this bitfield.
+     */
     public boolean isSet(long flag) {
         return (value & flag) == flag;
     }
 
+    /**
+     * Sets given flag(s) to 1.
+     * @param flag flag to set
+     */
     public void set(@NotNull V flag) {
         value |= flag.getValue();
     }
 
+    /**
+     * Sets given flag(s) to 1.
+     * @param flag1 flags to set
+     */
     public void set(@NotNull V flag1, @NotNull V flag2) {
         value |= flag1.getValue() | flag2.getValue();
     }
 
+    /**
+     * Sets given flag(s) to 1.
+     * @param flag1 flags to set
+     */
     public void set(@NotNull V flag1, @NotNull V flag2, @NotNull V flag3) {
         value |= flag1.getValue() | flag2.getValue() | flag3.getValue();
     }
 
+    /**
+     * Sets given flag(s) to 1.
+     * @param flag1 flags to set
+     */
     public void set(@NotNull V flag1, @NotNull V flag2, @NotNull V flag3, @NotNull V flag4) {
         value |= flag1.getValue() | flag2.getValue() | flag3.getValue() | flag4.getValue();
     }
 
+    /**
+     * Sets given flag(s) to 1.
+     * @param flags flags to set
+     */
     @SafeVarargs
     public final void set(@NotNull V @NotNull... flags) {
         for(V flag : flags)
