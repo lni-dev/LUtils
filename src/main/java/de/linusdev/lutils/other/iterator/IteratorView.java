1fee8733e3bec65c6f9920940d9ece67759e14fa
LinusDev <linus@linusdev.de>
2025-04-07 12:25:30 +0200

    added IteratorView

---
+++ src/main/java/de/linusdev/lutils/other/iterator/IteratorView.java
@@ -0,0 +1,28 @@
+package de.linusdev.lutils.other.iterator;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.function.Function;
+
+@SuppressWarnings("unused")
+public class IteratorView<R, E> implements Iterator<E> {
+
+    private final @NotNull Iterator<R> original;
+    private final @NotNull Function<R, E> converter;
+
+    public IteratorView(@NotNull Iterable<R> original, @NotNull Function<R, E> converter) {
+        this.original = original.iterator();
+        this.converter = converter;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return original.hasNext();
+    }
+
+    @Override
+    public E next() {
+        return converter.apply(original.next());
+    }
+}
